a8348ce141b94c223820dce612d139d1ca7d2cb5
eriky <erikyryang@gmail.com>
2023-04-11 23:31:38 -0300

    ajustes no client usado no trabalho

--- client.py
+++ client.py
@@ -2,23 +2,22 @@ import socket
 import random
 from time import sleep
 
-
 MIN = 10**9
-MAX = 10**29
+MAX = 10**30
+RESPONSE_SIZE = 10**2
 
 HOST = socket.gethostname();
-PORT = 5000
+PORT = 1234
 
 def send_receive():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, 1234))
+    client.connect((HOST, PORT))
 
-    random_value = random.randint(MIN, MAX)
-    print(random_value.__sizeof__())
-    client.sendall(str.encode(str(random_value)))
+    random_value = str(random.randint(MIN, MAX))
+    client.sendall(str.encode(random_value))
 
-    msg = client.recv(90)
-    print(msg.decode("utf-8"))
+    msg = client.recv(RESPONSE_SIZE)
+    print(msg.decode("utf-8") + " FIM")
 
 
 while True:
