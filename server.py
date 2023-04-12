0c6f0f3bb6efde8d49b64ecf2d10be71697eb958
eriky <erikyryang@gmail.com>
2023-04-12 00:09:10 -0300

    ajustes no servidor

--- server.py
+++ server.py
@@ -1,17 +1,36 @@
 import socket
+import string
+import random
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen()
+RESPONSE_SIZE = 10**2
+
+HOST = socket.gethostname();
+PORT = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((socket.gethostname(), 1234))
+server.listen()
+
+def random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 while True:
-    clientsocket, address = s.accept()
+    clientsocket, address = server.accept()
     print(f"Connection from {address} has been established!")
-    #clientsocket.send(bytes("Welcome to the Server", "utf-8"))
-    #print(address)
-    data = clientsocket.recv(1024)
-    st = (data.decode())
-    st = st.upper()
-    clientsocket.sendall(str.encode(data.decode().upper()))
+
+    client_data = clientsocket.recv(RESPONSE_SIZE)
+    received_number = (client_data.decode())
+
+    received_lenght = len(received_number)
+    if received_lenght > 10:
+        generated_string = random_string(received_lenght)
+        clientsocket.sendall(str.encode(generated_string))
+    else:
+        if received_number % 2 == 0:
+            clientsocket.sendall("PAR".encode())
+        else:
+            clientsocket.sendall("IMPAR".encode())
+
 clientsocket.close()
 s.close()
