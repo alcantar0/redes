97c137408c2e42676a59c1436a01a1568c528b8c
eriky <erikyryang@gmail.com>
2023-04-13 07:28:14 -0300

    simplificando client.py

--- client.py
+++ client.py
@@ -1,27 +1,17 @@
 import socket
 from time import sleep
-from random import (choice, randint as number_generator)
+from random import choice
 
-MAX = 10**29
 RESPONSE_SIZE = 10**2
 HOST = socket.gethostname();
 PORT = 1234
-
-def generate_number_by_lenght(min: int, max: int, size: int):
-    return str(number_generator(min, max))[:size]
-
-def generate_number():
-    number = number_generator(1,99)
-    less_or_equal_to_ten_digits = int(generate_number_by_lenght(0, 10**8,10))
-    greater_than_ten_digits = int(generate_number_by_lenght(10**10, 10**30,30))
-    sizes = [less_or_equal_to_ten_digits, greater_than_ten_digits]
-    return number * choice(sizes)
 
 def send_to_server():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
 
-    random_value = str(generate_number())[:30]
+    numbers = [121213,88754,4*10**11,3*10**29]
+    random_value = str(choice(numbers))
     client.sendall(str.encode(random_value))
     msg = client.recv(RESPONSE_SIZE)
 
